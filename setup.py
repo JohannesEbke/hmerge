a9cae06fc0e66f456e2dbd9d10fd6c7866542e5e
Peter Waller <peter.waller@cern.ch>
2011-06-04 15:19:23 +0100

    Bugfix to setup.py preventing correct installation of hmerge

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hmerge",
-    packages=find_packages(),
+    py_modules=["hmerge"],
     entry_points={
         "console_scripts" : [
             "hmerge=hmerge:main",
